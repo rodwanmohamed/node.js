d866a59e5d4312e6aa98ed32f5bd505f210cbe29
rodwanmohamed <mohamed.548@buckeyemail.osu.edu>
2024-01-20 17:21:58 -0500

    Assignment1.2

--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525f55d6d5a65139a70b5750acc35ed97ddb4912")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
